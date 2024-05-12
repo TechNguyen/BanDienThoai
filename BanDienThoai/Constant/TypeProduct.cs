2101696645328d5aa5cae4e1327ad49bd418895c
TechNguyen <ndt13102003@gmail.com>
2024-05-12 17:14:26 +0700

    Add project files.

---
+++ BanDienThoai/Constant/TypeProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BanDienThoai.Constant
+{
+	public static class TypeProduct
+	{
+
+		public static string DIENTHOAI { get; set; } = "DIENTHOAI";
+		public static string PHUKIEN { get; set; } = "PHUKIEN";
+
+	}
+}
---
+++ SQLQuery2.sql
@@ -0,0 +1,8 @@
+Alter table [dbo].[SANPHAM]
+alter column [TENSP] nvarchar(500)
+
+Alter table [dbo].[SANPHAM]
+add GIAKM float
+
+
+
